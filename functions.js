e780ec3311f239ecb2879f2830f43d379ece7d35
lililok <eshanu.sofia@yandex.ru>
2024-04-03 19:37:49 +0300

    add grid and hover coloring

--- functions.js
+++ functions.js
@@ -1,1 +1,17 @@
-const div = document.createElement("div");
+const container = document.getElementById("container");
+let row, square;
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+    for (let j = 0; j < 16; j++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        row.appendChild(square);
+        square.addEventListener('mouseover', function() {
+            square.style.backgroundColor = 'green';
+        });
+    }
+}
+
